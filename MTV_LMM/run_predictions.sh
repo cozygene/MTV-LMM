9121574d849af30cffedc69dbc2589435e021b5c
liashenhav <35780866+liashenhav@users.noreply.github.com>
2018-01-25 22:26:46 +0200

    Update run_predictions.sh

--- MTV_LMM/run_predictions.sh
+++ MTV_LMM/run_predictions.sh
@@ -1,7 +1,7 @@
 OTU_id="Data_files/ind_NEW.txt"
 time_points="Data_files/ind_t.txt"
 mkdir Data_files/Prediction
-mkdir Data_files/GRM_files
+mkdir Data_files/GRM_files_pred
 
 while IFS= read -r line
 
@@ -21,8 +21,8 @@ do
 	  	gzip Data_files/Kinship_mat_files/Kinship_3_bins_$t.grm
 	  	gzip Data_files/Kinship_mat_files/Kinship_3_bins_shuff_$t.grm
 
-		/home/cozygene/cozygene/software/GCTA_1.26/gcta64 --grm-gz Data_files/Kinship_mat_files/Kinship_3_bins_$t --make-grm --out Data_files/GRM_files/GRM_3_bins__1_$t
-		/home/cozygene/cozygene/software/GCTA_1.26/gcta64 --grm-gz Data_files/Kinship_mat_files/Kinship_3_bins_shuff_$t --make-grm --out Data_files/GRM_files/GRM_3_bins__2_$t
+		/home/cozygene/cozygene/software/GCTA_1.26/gcta64 --grm-gz Data_files/Kinship_mat_files/Kinship_3_bins_$t --make-grm --out Data_files/GRM_files_pred/GRM_3_bins__1_$t
+		/home/cozygene/cozygene/software/GCTA_1.26/gcta64 --grm-gz Data_files/Kinship_mat_files/Kinship_3_bins_shuff_$t --make-grm --out Data_files/GRM_files_pred/GRM_3_bins__2_$t
 
 		#Random effects only
 		/home/cozygene/cozygene/software/GCTA_1.26/gcta64 --reml --mgrm Data_files/multi_grm_files/multi_grm_$t.txt --pheno Data_files/Phen_files/phen_file__$number.txt --reml-pred-rand --qcovar Data_files/Fixed_effect_per_OTU/prev_t_1_times__$number.txt --reml-est-fix --out Data_files/Prediction/Prediction_1_time_point_FF_$number/GRM_OTUs_reml__$t
