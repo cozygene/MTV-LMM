2fb0ced4f021c1f9021ec90adccf7caf93e5afdc
liashenhav <35780866+liashenhav@users.noreply.github.com>
2019-06-28 20:27:18 -0700

    Update run_TE.sh

--- MTV_LMM/run_TE.sh
+++ MTV_LMM/run_TE.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -ex
 
-#mkdir -p ../results/allo_HSCT
+
+
+Rscript "Time_explainability/TE_step_1.R" $2 $3
+Time_explainability/TE_step_2.sh $1 
+Rscript "Time_explainability/TE_step_3.R" $2 $3
 
 
 
-# time # time a script
-Rscript "Time_explainability/TE_step_1.R"
-Time_explainability/TE_step_2.sh $(cat init.txt)
-Rscript "Time_explainability/TE_step_3.R"
-
-
-
-
-wait
