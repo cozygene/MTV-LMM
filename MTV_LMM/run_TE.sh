c8c9e1f6212f76eec989544aaf410907b459f795
liashenhav <35780866+liashenhav@users.noreply.github.com>
2019-06-28 20:33:32 -0700

    Update run_TE.sh

--- MTV_LMM/run_TE.sh
+++ MTV_LMM/run_TE.sh
@@ -4,6 +4,7 @@ set -ex
 
 
 Rscript "Time_explainability/TE_step_1.R" $2 $3
+chmod +x Time_explainability/TE_step_2.sh
 Time_explainability/TE_step_2.sh $1 
 Rscript "Time_explainability/TE_step_3.R" $2 $3
 
